68f79469279409e02a9d6227936549c0b184cbd1
Uzain95 <uzainali95@gmail.com>
2024-05-14 00:31:55 -0700

    using jsonresponse

--- drf_serialization/api/views.py
+++ drf_serialization/api/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from .models import Student
 from .serializers import StudentSerializers
 from rest_framework.renderers import JSONRenderer
-from django.http import HttpResponse
+from django.http import HttpResponse,JsonResponse
 # Create your views here.
 
 
@@ -10,8 +10,9 @@ from django.http import HttpResponse
 def student_details(request,pk):
     stu = Student.objects.get(id=pk)
     serializer = StudentSerializers(stu)
-    json_data = JSONRenderer().render(serializer.data)
-    return HttpResponse(json_data, content_type='application/json')
+    # json_data = JSONRenderer().render(serializer.data)
+    # return HttpResponse(json_data, content_type='application/json')
+    return JsonResponse(serializer.data)
 
 
 # Query Set - All Student Data
